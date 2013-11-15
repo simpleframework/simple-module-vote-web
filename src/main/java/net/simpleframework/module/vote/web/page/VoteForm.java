23eb10b095e09895b4f7a5338d3b81b22686389c
simpleframework <cknet@126.com>
2013-11-15 12:12:21 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/module/vote/web/page/VoteForm.java
+++ src/main/java/net/simpleframework/module/vote/web/page/VoteForm.java
@@ -41,8 +41,7 @@ import net.simpleframework.mvc.template.lets.FormTableRow_ListTemplatePage;
 /**
  * Licensed under the Apache License, Version 2.0
  * 
- * @author 陈侃(cknet@126.com, 13910090885)
- *         https://github.com/simpleframework
+ * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
 public class VoteForm extends FormTableRow_ListTemplatePage implements IVoteContextAware {
